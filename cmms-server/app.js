91912d2ef9dfb5d4bf8c50813b97eaba9184dab4
Basel <baselanani@outlook.com>
2025-03-24 01:49:24 -0500

    Modified database design.

--- cmms-server/app.js
+++ cmms-server/app.js
@@ -20,8 +20,7 @@ let con = sql.createPool({
 	port: process.env.DB_PORT,
 	user: process.env.DB_USER,
 	password: process.env.DB_PASSWORD,
-	database: process.env.DB_DATABASE,
-	supportBigNumbers: true
+	database: process.env.DB_DATABASE
 });
 
 const sessions = {};
